cd6071d90f963c2052f0e12c159a9fc73794aeb0
Aone <quanyi.zhao@ucsf.edu>
2017-11-03 22:50:31 -0700

    Nov 3

--- adapt_trim.py
+++ adapt_trim.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-###########################################################
-# Usage: adapt_trim.py --single/paired_end Reads1|Reads2  #
-# Manual: trim RAW reads by CUTADAPT                      #
-###########################################################
+###############################################################################
+# Usage: adapt_trim.py --single_end/paired_end --truseq/nextra Reads1|Reads2  #
+# Manual: trim RAW reads by CUTADAPT                					      #
+###############################################################################
 import sys,os
 import getopt
-optlist,args = getopt.getopt(sys.argv[1:],'h',["single_end","paired_end"])
+optlist,args = getopt.getopt(sys.argv[1:],'h',["single_end","paired_end","truseq","nextra"])
 
 ########## subroutine ##########
 # help_message()    print help message for this pipeline
@@ -14,29 +14,38 @@ def help_message():
 Usage:  %s [options*] <Rq.fastq>|<.R2.fastq>
 Options:
 -h|--help           print this help message
---single_end        single end sequence mapping
---paired_end        paired end sequence mapping
+--single_end        single end sequence 
+--paired_end        paired end sequence 
+--truseq			Truseq adapters (default), full sequence
+--nextra			Nextra adapters, transposase sequence 
 ##########''' % sys.argv[0]
 
 #def single end algorithm
 class trim_adapt:
-	def __init__(self,x):
+	def __init__(self,x,m):
 		self.left = '%s' % x
 		self.nx = x.rsplit('/',1)[-1].rsplit('.',1)[0]
 		self.out_l = '-o %s_trimed.gz' % self.nx
 		self.read_type = '-f fastq'
 		self.kept_len = '-m 20'
-		self.adapt_l = '-a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
-		self.adapt_r = '-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
-	
+		
+		### TruSeq Index:
+		if m = 'truseq':
+			self.adapt_l = '-a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC'
+			self.adapt_r = '-A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT'
+		### Nextra Index:
+		elif m = 'nextra':
+			self.adapt_l = '-a CTGTCTCTTATACACATCT'
+			self.adapt_r = '-A CTGTCTCTTATACACATCT'
+		
 	def single_end(self):
-		return 'cutadapt %s %s %s %s %s' % (self.read_type, self.kept_len, self.adapt_l, self.out_l, self.left)
+		return 'cutadapt %s %s %s %s %s > %s_cutadapt.log' % (self.read_type, self.kept_len, self.adapt_l, self.out_l, self.left, self.nx)
 
 	def paired_end(self,y):
 		self.right = '%s' % y
 		self.ny = y.rsplit('/',1)[-1].rsplit('.',1)[0]
 		self.out_r = '-p %s_trimed.gz' % self.ny
-		return 'cutadapt %s %s %s %s %s %s %s %s' % (self.read_type, self.kept_len, self.adapt_l, self.adapt_r, self.out_l, self.out_r, self.left, self.right)
+		return 'cutadapt %s %s %s %s %s %s %s %s > %s_cutadapt.log' % (self.read_type, self.kept_len, self.adapt_l, self.adapt_r, self.out_l, self.out_r, self.left, self.right, self.nx)
 
 ######MAIN#########
 if __name__ == '__main__':
@@ -44,18 +53,26 @@ if __name__ == '__main__':
 		if opt in ('-h'):
 			help_message()
 			sys.exit(0)
-			
-		core = trim_adapt(args[0])
 		
+		seq_mod = 'truseq'
+		if opt in ('--truseq'):
+			core = trim_adapt(args[0], seq_mod)
+			
+		elif opt in ('--nextra'):
+			seq_mod ='nextra'
+			core = trim_adapt(args[0], seq_mod)
+			
 		if opt in ('--single_end'):
 			cmd = core.single_end()
+			print "cutadapt is running with SE, %s mode!" % seq_mod.capitalize()
 			os.system(cmd)
 
-		if opt in ('--paired_end'):
+		elif opt in ('--paired_end'):
 			cmd = core.paired_end(args[1])
+			print "cutadapt is running with PE, %s mode!" % seq_mod.capitalize()
 			os.system(cmd)
 		
 ################ END ################
 #          Created by Aone          #
-#       Quanyi.Zhao@ucsf.edu        #
+#       quanyiz@stanford.edu        #
 ################ END ################
