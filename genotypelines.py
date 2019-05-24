404f3d94e383f1e81518e90d745b16370fc32227
Aone <quanyiz@stanford.edu>
2019-05-24 10:43:03 -0700

    Update genotypelines.py

--- genotypelines.py
+++ genotypelines.py
@@ -16,7 +16,7 @@ def get_geno(r,v,o):
 	shell.close()
 	os.system('bash shell.sh')
 
-def get_lines(f,m):
+def get_lines(f):
 	for line in open(f):
 		a = line.split()
 		if line[0:2] != '##':
@@ -40,11 +40,10 @@ def get_lines(f,m):
 def main():
 	parser = argparse.ArgumentParser(description="Search rsID and get heterozygous/homozygous  lines in the VCF file")
 	parser.add_argument("-v", "--vcf", help="(gzipped) genotypes VCF file", default='//nfs/baldar/pooled_genotypes/imputed/snps_only/snps_only.vcf.gz')
-	#parser.add_argument("-m", "--mode", help="heterozygous/homozygous", choices=['1|1','1|0','0|1','0|0'], default='1|1')
 	parser.add_argument('rs', help="input SNP, a rsID per line")
 	args = parser.parse_args()
 	get_geno(args.rs, args.vcf, './tmp')
-	get_lines('./tmp', args.mode)
+	get_lines('./tmp')
 	os.system('rm ./tmp ./shell.sh')
 
 if __name__ == '__main__':
