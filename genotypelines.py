7e615ea3821812ac88e002e60537572f9320e9c0
Aone <quanyiz@stanford.edu>
2019-06-17 13:45:47 -0700

    Update genotypelines.py

--- genotypelines.py
+++ genotypelines.py
@@ -16,7 +16,7 @@ def get_geno(r,v,o):
 	shell.close()
 	os.system('bash shell.sh')
 
-def get_lines(f):
+def get_lines(f,m):
 	for line in open(f):
 		a = line.split()
 		if line[0:2] != '##':
@@ -30,7 +30,7 @@ def get_lines(f):
 				res = []
 				for i in range(9,len(a)):
 					geno = a[i].split(":")[0]
-					if  geno == '0|1' or geno == '1|0':
+					if  geno in m:
 						cell.append(i)
 				for i in cells:
 					if cells[i] in cell:
@@ -42,9 +42,18 @@ def main():
 	parser = argparse.ArgumentParser(description="Search rsID and get heterozygous/homozygous  lines in the VCF file")
 	parser.add_argument("-v", "--vcf", help="(gzipped) genotypes VCF file", default='//nfs/baldar/pooled_genotypes/imputed/snps_only/snps_only.vcf.gz')
 	parser.add_argument('rs', help="input SNP, a rsID per line")
+	parser.add_argument('-g', '--geno', choices=['ref', 'alt', 'het'], help="genotypes, ref=refernce allele homozygous, alt=alternative allele homozygous, het=heterozygous(default).", default='het')
 	args = parser.parse_args()
 	get_geno(args.rs, args.vcf, './tmp')
-	get_lines('./tmp')
+	if args.geno == 'ref':
+		mode = ['0|0', '0/0']
+	elif args.geno == 'alt':
+		mode = ['1|1', '1/1']
+	elif args.geno == 'het':
+		mode = ['1|0', '0|1', '1/0', '0/1']
+	else:
+		print("genotypes must be ref/alt/het")		
+	get_lines('./tmp', mode)
 	os.system('rm ./tmp ./shell.sh')
 
 if __name__ == '__main__':
